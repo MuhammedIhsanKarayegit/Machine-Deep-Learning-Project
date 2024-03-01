948be779256684a54d1089d3d0a484d2942f249f
MuhammedIhsanKarayegit <104142526+MuhammedIhsanKarayegit@users.noreply.github.com>
2024-03-01 18:38:18 +0300

    Commit

--- Face Detection with Haar Cascades/Code/Code.py
+++ Face Detection with Haar Cascades/Code/Code.py
@@ -1,18 +1,20 @@
 import cv2 as cv
 
+# Kamera açma ve daha önceden tanımlanmış olan xml dosyasını okuma işlemlerini bu noktada gerçekleştiriyoruz.
 cap = cv.VideoCapture(0)
 haar_cacade = cv.CascadeClassifier('Xml Files\haar_face.xml')
 
 while True:
-
+    # Kameradan alınan görünütüyü okuma
     ret, fream =  cap.read()
-
+    # Görseldeki gürültüyü azaltmak için görseli gri renkli bir yapıya çeviriyoruz
     gray = cv.cvtColor(fream, cv.COLOR_BGR2GRAY)
-    faces_rect = haar_cacade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
+    # Haar cascade classifier ile yüzleri belirleyip bunları bulduğu kısımlara sınırlama koyuyoruz.
+    faces_rect = haar_cacade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+    # Bulunan her yüz için bir dikdörtgen oluşturuyoruz.
     for (x, y, w, h) in faces_rect:
         cv.rectangle(fream, (x, y),(x+w, y+h), (0,255,0),thickness=2)
-    
+    # Oluşturmuş olduğumuz bu dikdörtgenleri ekrana çıkartıyoruz
     cv.imshow('Detected Faces', fream)
 
 
